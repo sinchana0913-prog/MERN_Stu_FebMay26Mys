56673491f1f8b012a3826b27d0b737a89c9d8df4
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-03 15:43:53 +0530

    Comparison operators added

---
+++ W3/D2/P2.js
@@ -0,0 +1,11 @@
+//Comparison Operators
+console.log("Comparison Operators");
+const x = 3, y = 4;
+console.log("x==y: ",x==y);// Loose equality
+console.log("x===y: ",x===y);// Strict equality
+console.log("x!=y: ",x!=y);// Loose equality
+console.log("x!==y: ",x!==y);// Strict equality
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);
