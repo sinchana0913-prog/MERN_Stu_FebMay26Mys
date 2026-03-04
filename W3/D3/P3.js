d7699afae6ae714035b6b5405386ba0f9f986492
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-04 12:39:00 +0530

    break and continue statement added

---
+++ W3/D3/P3.js
@@ -0,0 +1,15 @@
+for(let i=0;i<5;i++){ // break statement
+    if(i===2){
+    console.log("i",i); 
+    break;
+    }
+}
+
+//continue
+for(let i=0;i<5;i++){ // continue statement
+    if(i===2){
+    //console.log("i",i); 
+    continue;
+    }
+    console.log("i",i); 
+}
