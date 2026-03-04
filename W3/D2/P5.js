d953a9bc5587bb658cf5dba8ffee5d5b1fc89062
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-04 11:14:19 +0530

    Function declaration added

---
+++ W3/D2/P5.js
@@ -0,0 +1,9 @@
+// Function Declaration
+// function greet(name);
+greet("sinchana");
+// function declaration/definition
+function greet(name) {
+    console.log("Hello, "+name+" !");
+}
+//Function call
+greet("sinchana");
