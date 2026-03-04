c26c940913d842bdc45c6b9dd2e792199ce07378
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-04 12:38:08 +0530

    while added

---
+++ W3/D3/P2.js
@@ -0,0 +1,21 @@
+// While loop
+console.log("While loop");
+
+let count = 0; // forward while loop
+while(count<5){
+    console.log("count: ",count);
+    count++;
+}
+
+let i = 5; // backward while loop
+while(i>0){
+    console.log("i: ",i);
+    i--;
+}
+// do while
+let value = 0;
+do{
+    console.log("Value:",value);
+    value++;
+}while(value < 3);
+
