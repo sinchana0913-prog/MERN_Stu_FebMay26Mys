ae464b65c33f2e3965e8d2290dc83f7664073706
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-04 13:39:04 +0530

    Basics of Object added

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -4,6 +4,24 @@ const person = {
     age:21,
     isStudent: false
 };
-//onsole.log("person",person);
+//console.log("person",person);
 console.log("Name:",person.name);
 console.log("Name:",person["age"]);
+
+//Add a new property
+person.city = "Mysore";
+console.log("person",person);
+//Modify
+person.age = 31;
+//delete
+delete person.isStudent;
+console.log("person",person);
+
+//Object constructor
+const car = new Object();
+car.make = "Audi";
+car.model = "A4";
+car.year =  2026;
+console.log("car:",car);
+
+
