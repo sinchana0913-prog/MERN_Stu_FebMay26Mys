61ccc1e89d4131094cbddd219c8f99505a4471f9
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-04 12:40:26 +0530

    Basics of Objects added

---
+++ W3/D3/P5.js
@@ -0,0 +1,9 @@
+// Basics of Objects
+const person = {
+    name:"sinchana",
+    age:21,
+    isStudent: false
+};
+//onsole.log("person",person);
+console.log("Name:",person.name);
+console.log("Name:",person["age"]);
