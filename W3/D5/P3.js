f164d1b3fda0ee04f4c363d59815de159bfccb12
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-08 10:29:05 +0530

    Mouseover event added

---
+++ W3/D5/P3.js
@@ -0,0 +1,11 @@
+// Mouse Events
+const hoverBox = document.getElementById("hoverBox");
+
+hoverBox.addEventListener("mouseover",function(){
+    hoverBox.textContent = "Mouse is over me";
+    hoverBox.style.backgroundColor = "lightyellow";
+});
+hoverBox.addEventListener("mouseout",function(){
+    hoverBox.textContent = "Mouse is over me";
+    hoverBox.style.backgroundColor = "";
+});
