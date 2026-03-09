92eff75e389c3bf95b0bee872aef56208cce1255
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-09 17:40:25 +0530

    Logging added

---
+++ W4/D1/P4.js
@@ -0,0 +1,26 @@
+//Logging
+
+console.log("Console logging");
+
+console.warn("Warning message");
+
+console.error("Error message");
+
+let users = [
+    {id:1,name:"Amit"},
+    {id:2,name:"Ammu"},
+];
+console.log(users);
+console.table(users);
+
+//Group related logs
+console.group("Grouped logs");
+console.log("Log 1");
+console.log("Log 2");
+console.log("Log 3");
+console.groupEnd();
+
+//measure execution time
+console.time("LoopTimer");
+for(let i = 0; i<1000; i++){}
+console.timeEnd("LoopTimer");
