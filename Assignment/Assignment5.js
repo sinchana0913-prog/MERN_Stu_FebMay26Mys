663c346632de6ecd5bd6abad5e3f37a3d37c8ebb
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-12 13:54:13 +0530

    wallet added

---
+++ Assignment/Assignment5.js
@@ -0,0 +1,14 @@
+const wallet = {
+  owner: "Alice",
+  balance: 1000,
+  lastTransaction: null,
+
+  deposit: function(amount) { 
+  },
+  withdraw: function(amount) {
+  }
+};
+wallet.deposit(500);
+wallet.withdraw(200);
+
+console.log(wallet);
