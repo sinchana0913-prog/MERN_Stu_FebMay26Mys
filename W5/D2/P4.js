d5be931560616f345505a0e486986657f04477e1
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-17 13:32:03 +0530

    Writing custom callback functions added

---
+++ W5/D2/P4.js
@@ -0,0 +1,31 @@
+// Writing custom callback functions
+
+function processStudent(name,score,callback,monkey){
+    console.log("Student name:",name);
+    console.log("Score:",score);
+
+    callback(name,score);
+    monkey(name,score);
+}
+function showResult(name,score){
+    if(score >= 70){
+        console.log(name+ " has passed.");
+    }
+    else{
+        console.log(name+ " has failed.");
+    }
+}
+function showGrade(name,score){
+    if(score >= 85){
+        console.log("Grade:A+");
+    }
+    else if(score >= 75){
+        console.log("Grade:B");
+    }
+    else{
+        console.log("Grade:Fail");
+    }
+}
+
+processStudent("sinchana",89,showResult,showGrade);
+//processStudent("sinchana",89,showGrade);
