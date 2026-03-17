f733422ded21d0dd0d14bf3e845fb01c7fd7f094
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-17 13:32:38 +0530

    Callback error added

---
+++ W5/D2/P5.js
@@ -0,0 +1,19 @@
+//Callback error
+
+function divideNumbers(a,b,callback){
+    if(b==0){
+        callback("Cannot divide by zero.",null);
+        return;
+    }
+    const result = a/b;
+    callback(null,result);
+
+}
+
+divideNumbers(10,2,function(error,result){
+    if(error){
+        console.log("Error: ",error);
+        return;
+    }
+    console.log("Result:",result);
+});
