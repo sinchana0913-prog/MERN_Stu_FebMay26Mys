78388548e75ebba04339bf8e2cd2acae0982d75a
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-18 10:41:54 +0530

    Handling errors with try/catch added

---
+++ W5/D3/P3.js
@@ -0,0 +1,24 @@
+//Handling errors with try/catch
+
+function loadCustomerProfile(){
+    return new Promise(function(resolve,reject){
+        const isServiceAvailable = false;
+
+        if(isServiceAvailable){
+            resolve("Success! Customer profile loaded.");
+        }
+        else{
+            reject("Unsuccessful!! Customer profile unavailable.");
+        }
+    });
+}
+async function showCustomerProfile(){
+    try{
+       const message = await loadCustomerProfile();
+    console.log(message); 
+    }
+    catch(error){
+        console.error("Error:",error);
+    }
+}
+showCustomerProfile();
