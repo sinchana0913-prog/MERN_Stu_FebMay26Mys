a80916be016af8b84a399efbd306fd98f405bb20
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-18 11:27:52 +0530

    combining multiple promise-based steps with async/await added

---
+++ W5/D3/P4.js
@@ -0,0 +1,30 @@
+//combining multiple promise-based steps with async/await
+
+function getOrderId(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(501);
+        },501);
+
+    });
+}
+function getOrderDetails(orderId){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({
+                id:orderId,
+                product:"Laptop",
+                quantity:2
+            });
+        },700);
+    });
+}
+async function showOrderSummmary(){
+    const orderId = await getOrderId();
+    console.log("Order id:",orderId);
+
+    const orderDetails = await getOrderDetails(orderId);
+    console.log("Product:",orderDetails.product);
+    console.log("Quantity:",orderDetails.quantity);
+}
+showOrderSummmary();
