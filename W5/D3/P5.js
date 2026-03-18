f6d434270554ef76a6eb3350790489a1f87598c8
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-18 12:25:33 +0530

    an async function always returns a promise added

---
+++ W5/D3/P5.js
@@ -0,0 +1,9 @@
+//an async function always returns a promise
+async function getStatusMessage(){
+    return "order is ready";
+}
+const result = getStatusMessage();
+console.log("Is this returned value a promise?",result instanceof Promise);
+result.then(function(message){
+    console.log("Resolved value:",message);
+});
