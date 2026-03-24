acee8136bfeb041baac67cf8c321aa08aeb08670
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-24 10:52:45 +0530

    Event loop with multiple asynchronous tasks

---
+++ W6/D2/P3.js
@@ -0,0 +1,15 @@
+//Event loop with multiple asynchronous tasks
+console.log("Main script started.");
+//task 1
+setTimeout(() => {
+    console.log("Timer A finished after 500ms");
+},500);
+//task 2
+setTimeout(() => {
+    console.log("Timer B finished after 100ms");
+},100);
+//task 3
+setTimeout(() => {
+    console.log("Timer C finished after 0ms but it still wait for sync code to complete");
+},0);
+console.log("Main script ended.");
