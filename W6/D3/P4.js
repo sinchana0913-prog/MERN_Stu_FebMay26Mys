99a89bad6ea9daa180c30866dbaedd57e1e95450
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-25 13:44:28 +0530

    Reading & writing files asynchronously with fs/promises

--- W6/D3/P4.js
+++ W6/D3/P4.js
@@ -6,7 +6,7 @@ const path = require("path");
 async function runPromiseBasedFileFlow(){
  const filePath = path.join(__dirname,"promises-node.txt");
  try{
-    await fs.writeFile(filePath,"written using fs/promises.this work with async/await");
+    await fs.appendFile(filePath,"written using fs/promises.this work with async/await");
     console.log("File written using fs/promises");
 
     const content = await fs.readFile(filePath,"utf-8");
