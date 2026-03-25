c54df439117caa9a23a8ce27e67fdc804da09849
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-25 11:55:17 +0530

    Directory(folder) operations: mkdir, readdir and rmdir/rm

---
+++ W6/D3/P6.js
@@ -0,0 +1,17 @@
+//Directory(folder) operations: mkdir, readdir and rmdir/rm
+const fs = require("fs")
+const path = require("path");
+
+const demoDir = path.join(__dirname,"demo-folder");
+//create folder
+fs.mkdirSync(demoDir,{recursive:true});
+
+fs.writeFileSync(path.join(demoDir,"File1.txt"),"File1 content");
+fs.writeFileSync(path.join(demoDir,"File2.txt"),"File2 content");
+
+//to check folder contents
+const entries = fs.readdirSync(demoDir);
+console.log("Directory contents: ",entries);
+
+fs.rmSync(demoDir,{recursive:true,force:true});
+console.log("Directory exists?",fs.existsSync(demoDir));
