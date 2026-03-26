9821795cd2bacd537ae821d6cc60d4332dd9f1c5
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-26 17:58:38 +0530

    Parsing query parameters

---
+++ W6/D4/P7.js
@@ -0,0 +1,25 @@
+//Parsing query parameters
+const http = require("http");
+
+const {URL} = require("url");
+const server = http.createServer(function (req, res) {
+    const fullUrl = new URL(req.url,"http://localhost:3001");
+    if(req.method =="GET" && fullUrl.pathname ==="/search"){
+        const term = fullUrl.searchParams.get("term");
+        const page = fullUrl.searchParams.get("page");
+
+        res.writeHead(200,{"content-type":"application/json"});
+        res.end(JSON.stringify({
+            route:"/search",
+            term:term,
+            page:page
+        }));
+        return;
+    }
+    res.writeHead(404,{"Content-Type":"application/json"});
+    res.end(JSON.stringify({success:false,messsage:"Route not found."}));
+});
+server.listen(3001,function(){
+    console.log("server is runinng at http://localhost:3001");
+
+});
