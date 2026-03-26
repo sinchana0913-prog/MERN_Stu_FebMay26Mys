bfa409a792c5a80603c8f8be69da72a9868a9154
sinchana0913-prog <sinchana0913@gmail.com>
2026-03-26 17:58:14 +0530

    sending response in json format

---
+++ W6/D4/P6.js
@@ -0,0 +1,23 @@
+// sending response in json format
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+    if (req.url === "/api/status" && req.method === "GET") {
+        const responseData = {
+            success: true,
+            message: "API is working",
+            server: "NodeJS HTTP Module"
+        };
+        //JSON responses using application/json
+        res.writeHead(200,{"Content-Type":"application/json"});
+        //json.stringify() converts JS objects into a JSON string
+        res.end(JSON.stringify(responseData));
+        return;
+    }
+    res.writeHead(404,{"Content-Type":"application/json"});
+    res.end(JSON.stringify({success:false,messsage:"Route not found."}));
+});
+server.listen(3001,function(){
+    console.log("server is runinng at http://localhost:3001");
+
+});
