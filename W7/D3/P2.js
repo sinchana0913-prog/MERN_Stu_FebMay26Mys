8ee1cd74c94ec4b000d8a06f528bfc443311c9b9
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-01 10:33:23 +0530

    Handling asynchronous errorswith callbacks

---
+++ W7/D3/P2.js
@@ -0,0 +1,28 @@
+//Handling asynchronous errorswith callbacks
+
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+app.get("/file",function(req,res,next){
+    const filePath = path.join(__dirname,"newFile.txt");
+    fs.readFile(filePath,"utf8",function(error,data){
+        if(error){
+            //Forward the async error
+            return next(error);
+        }
+        res.send(data);
+    });
+});
+//centralized error handling middleware
+app.use(function(error,req,res,next){
+    res.status(404).json({
+        success:false,
+        message:"File/Folder does not exist."
+       // Originalmessage:error.message
+    });
+});
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
