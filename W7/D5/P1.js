c2d799bb7a5e3e8f54983d192bacfb9a2cf0d510
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-03 11:36:19 +0530

    How cookie is used to track session ID

---
+++ W7/D5/P1.js
@@ -0,0 +1,13 @@
+//How cookie is used to track session ID
+//simulated browser server-side session store
+const sessionStore = {
+    "abc123":{
+        userId: 101,
+        username: "sinchana",
+        role:"admin"
+    }
+};
+//simulated browser cookie value
+const browserCookieSessionId = "abc123";
+const sessionData = sessionStore[browserCookieSessionId];
+console.log("Server-side session data:",sessionData);
