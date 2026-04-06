9ffdca4a6d76b992df233600ec16278bc66adda7
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-06 10:50:19 +0530

    callback baed validation functions

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/validator.js
@@ -0,0 +1,30 @@
+//callback baed validation functions
+
+function validateMovieSelection(movies,movieId,callback){
+    const selectedMovie = movies.find((movie)=>movie.id ===movieId);
+
+    if(!selectedMovie) {
+        return callback("Invalid movie selection. Choose a valid movie ID.",null);
+    }
+    callback(null,selectedMovie);
+}
+function validateTimeSelection(movie,selectedTime,callback){
+    const selectedShowTime = movie.showTimes.find((show)=>show.time.toLowerCase()===selectedTime.toLowerCase());
+
+    if(!selectedShowTime) {
+        return callback("Invalid time slot selection. Choose a valid time slot.",null);
+    }
+    callback(null,selectedShowTime);
+}
+function validateseatCount(seatCount,callback){
+    if(!isNaN(seatCount)|| seatCount <=0) {
+        return callback("Invalid seat count.Enter a valid seat count.",null);
+    }
+    callback(null,seatCount);
+}
+
+module.exports = {
+    validateMovieSelection,
+    validateTimeSelection,
+    validateseatCount
+};
