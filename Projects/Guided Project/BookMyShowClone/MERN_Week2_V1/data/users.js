ad9da948ee973ead9eacaf30c159e7591e165437
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-08 10:06:58 +0530

    User's details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,18 @@
+//User's details of the application
+const users = [
+    {
+        id:1,
+        name:"Sinchana",
+        email:"u@e.com",
+        password:"sin123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    }
+];
+module.exports = users;
