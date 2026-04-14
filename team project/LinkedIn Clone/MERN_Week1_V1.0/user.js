38f06b4dcf351681f073821d24dbb11f10e29533
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-14 11:06:38 +0530

    Manage user-related operation

---
+++ team project/LinkedIn Clone/MERN_Week1_V1.0/user.js
@@ -0,0 +1,56 @@
+//Manage user-related operation
+let users = [];
+let currentUser = null;
+
+// Create Profile
+function createUser(name, headline) {
+    const user = {
+        id: users.length + 1,
+        name,
+        headline,
+        skills: [],
+        experience: [],
+        education: []
+    };
+
+    users.push(user);
+    currentUser = user;
+    return user;
+}
+
+// Login
+function loginUser(id) {
+    const user = users.find(u => u.id === id);
+    if (user) {
+        currentUser = user;
+        return user;
+    }
+    return null;
+}
+
+function getCurrentUser() {
+    return currentUser;
+}
+
+function getAllUsers() {
+    return users;
+}
+
+module.exports = {
+    createUser,
+    loginUser,
+    getCurrentUser,
+    getAllUsers
+};
+// Get all users except current user
+function getOtherUsers() {
+  console.log("users",users);
+  console.log("currentUsers", currentUser);
+  
+  const others = users.filter((u) => u.id !== currentUser.id);
+  console.log("others ",others);
+  
+    return others;
+}
+
+module.exports.getOtherUsers = getOtherUsers;
