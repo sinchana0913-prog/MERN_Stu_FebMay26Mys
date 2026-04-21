39f0f22e65f52d738746e15eeb4979d6ecc2e9fe
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-21 10:05:12 +0530

    Load environment variables

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
-    console.log(`Server ruuning on port ${PORT}`);
+    console.log(`Server ruuning on port ${PORT}`); 
 });
