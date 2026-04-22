ce840225ab7cd763181724f305e7b4c9e81d5ae7
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-22 14:36:08 +0530

    created CRUD functions for movie and exported it

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/movie.service.js
@@ -0,0 +1,75 @@
+ const movie = require("../models/Movie");
+
+//Create Movie
+exports.createMovie = async (data) =>{
+    return await Movie.create(data);
+};
+
+//Get movies
+exports.getMovies = async(query)=>{
+    let { page = 1, limit = 5, genre, rating, search, sort} = query;
+
+    page = Number(page);
+    limit = Number(limit);
+
+    const filter = {isActive: true};
+
+    if(genre){
+        filter.genre = genre;
+    }
+    if(rating){
+        filter.rating = {$gte: Number(rating)};
+    }
+    if(search){
+        filter.$text = {$search:search};
+    }
+    let mongoQuery = Movie.find(filter);
+
+    if(sort){
+        mongoQuery = mongoQuery.sort(sort);
+    }
+    else{
+        mongoQuery = mongoQuery.sort("-createdAt");
+    }
+    const skip = (page-1)*limit;
+    mongoQuery = mongoQuery.skip(skip).limit(limit);
+
+    const movies = await mongoQuery;
+    const total = await Movie.countDocuments(filter);
+
+    return{
+        movies,
+        pagination:{
+            page,
+            limit,
+            total,
+        },
+    };
+
+
+};
+
+//update movie
+exports.updateMovie = async(Id,data)=>{
+    const movie = await Movie.findByIdUpdate(Id,data,{
+        new:true,
+        runValidators:true,
+    });
+
+    if(!movie)
+        throw new Error("Movie not found");
+
+        return movie;
+    
+};
+
+//Delete movie
+exports.deleteMovie = async (id) =>{
+    //soft delete
+    const movie = await Movie.findByIdUpdate(Id,{
+        isActive:false,
+        
+    });
+    if(!movie)
+        throw new Error("Movie not found");
+};
