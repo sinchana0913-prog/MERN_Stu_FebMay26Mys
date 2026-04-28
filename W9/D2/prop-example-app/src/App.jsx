6a08367556751f005e9019ae678c9c8607347640
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-28 15:29:06 +0530

    props basics react app created

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {ProBasics, PropBasics} from "./components/P1"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <PropBasics/>
+    </>
+  )
+}
+
+export default App
