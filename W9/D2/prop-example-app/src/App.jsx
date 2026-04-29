882668cd6fda0375dba90f190904c3347f49976d
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-29 12:45:06 +0530

    objects and array props added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -3,6 +3,8 @@ import {PropBasics} from './components/P1'
 import { PropsChildren } from './components/P3'
 import{PropDrillingDemo} from './components/P4'
 import { SharedStateParent } from './components/P5'
+import { CompositionDemo } from './components/P6'
+import { ObjectsAndArrayProps } from './components/P7'
 import './App.css'
 //import { PropDestructuring } from './components/P2'
 
@@ -15,7 +17,9 @@ function App() {
    {/* <PropDestructuring /> */}
    {/*<PropsChildren/>*/}
    {/*<PropDrillingDemo/>*/}
-   <SharedStateParent/>
+   {/*<SharedStateParent/>*/}
+   {/*<CompositionDemo/>*/}
+   <ObjectsAndArrayProps/>
     </>
   )
 }
