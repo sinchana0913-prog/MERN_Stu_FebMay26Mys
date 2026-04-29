d1db9886b47e5d71dd409e6eb168702e4f023597
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-29 11:43:48 +0530

    Prop drilling component added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-import {ProBasics, PropBasics} from "./components/P1"
+import {PropBasics} from './components/P1'
+import { PropsChildren } from './components/P3'
+import{PropDrillingDemo} from './components/P4'
+import { SharedStateParent } from './components/P5'
 import './App.css'
+//import { PropDestructuring } from './components/P2'
 
 function App() {
   
 
   return (
     <>
-     <PropBasics/>
+    {/*<PropBasics/>*/}
+   {/* <PropDestructuring /> */}
+   {/*<PropsChildren/>*/}
+   {/*<PropDrillingDemo/>*/}
+   <SharedStateParent/>
     </>
   )
 }
