9bbbdc822253a2f12462219994e0175a130fc694
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-30 10:23:07 +0530

    morgan added

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -2,13 +2,17 @@ const  express = require("express");
 const authRoutes = require("./src/routes/auth.routes");
 const movieRoutes = require("./src/routes/movie.routes");
 const bookingRoutes = require("./src/routes/booking.routes");
+const errorMiddleware = require("./src/middleware/error.middleware");
 const showRoutes = require("./src/routes/show.routes");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/movies",movieRoutes);
 app.use("/api/bookings",bookingRoutes);
+app.use(morgan("dev"));
+app.use(errorMiddleware);
 app.use("/api/shows",showRoutes);
 //Base URL 
 app.get("/",(req,res)=>{
