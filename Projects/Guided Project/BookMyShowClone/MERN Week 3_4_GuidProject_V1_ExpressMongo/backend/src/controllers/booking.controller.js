1b91a15cdd7957446fa21ff428cabda130f44560
sinchana0913-prog <sinchana0913@gmail.com>
2026-04-30 10:04:55 +0530

    controller for creation, cancel & get bookings is added

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/booking.controller.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/booking.controller.js
@@ -40,7 +40,7 @@ exports.cancelBooking = async (req,res,next) => {
         res.status(200).json({
             success:true,
             message:"Bookings cancelled.",
-            data:bookings,
+            
         });
     }
     catch(error){
