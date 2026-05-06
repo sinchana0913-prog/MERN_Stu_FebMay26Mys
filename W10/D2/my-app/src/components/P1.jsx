5a6d7a3f98bd6bbdc6e12e34800100475271aa93
sinchana0913-prog <sinchana0913@gmail.com>
2026-05-06 10:29:20 +0530

    useContext hook

---
+++ W10/D2/my-app/src/components/P1.jsx
@@ -0,0 +1,75 @@
+//useContext hook
+//context:lets you share data between components
+//without passing props manually through every level
+import { createContext,useContext,useState } from "react";
+//why to use useContext?
+//1.Helps us avoid prop drilling
+//2.useful for shared values:
+//-theme details
+//-logged in user details
+//-language settings
+//-app settings
+
+//Basics steps:
+//1.create a context
+//2.wrap components with provider
+//3.Read values using useContext()
+
+const ThemeContext = createContext();
+
+//child component 1
+function Header(){
+    const theme = useContext(ThemeContext);
+    return(
+        <header style={{
+            padding:'20px',
+            marginTop:'20px',
+            background:theme === 'dark'?'#222':'#eee',
+            color:theme === 'dark' ? '#fff' : '#000'
+        }}>
+          <h3>Header component</h3>
+          <p>Current theme from context:{theme}</p>
+        </header>
+    );
+}
+//child component 2
+function Content(){
+    const theme = useContext(ThemeContext);
+    return(
+        <div style={{
+            padding:'20px',
+            marginTop:'20px',
+            background:theme === 'dark'?'#333':'#f9f9f9',
+            color:theme === 'dark' ? '#fff' : '#000'
+        }}>
+            <p>This component also uses the same context value</p>
+
+        </div>
+    );
+}
+function Layout(){
+    return(
+        <div>
+            <Header/><Content/>
+        </div>
+    );
+}
+export function UseContextIntro(){
+    //shared state
+    const [theme, setTheme] = useState('light'); // 'light' is the starting value
+    const toggleTheme = ()=>{
+        setTheme(theme ==='light'?'dark':'light');
+    };
+    return(
+        <section>
+            <h2>useContext Example via theme</h2>
+            <p>
+                Example to show the usage of context sharing
+            </p>
+            <button onClick={toggleTheme}>Toggle Theme</button>
+            <ThemeContext Provider value={theme}>
+                <Layout/>
+            </ThemeContext>
+        </section>
+    );
+}
