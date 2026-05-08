6cfe73c42c078da4aff213da0b6ad2201202cfe0
https://github.com/sinchana0913-prog/myprofile <sinchana0913@gmail.com>
2026-05-08 11:50:03 +0530

    Accessibility for custom Clickable elements

---
+++ W10/D3/my-event-app/src/components/P10.jsx
@@ -0,0 +1,22 @@
+// Accessibility for custom Clickable elements
+// to make non-button elements behave like a accessible button
+
+export function Accessibility(){
+    const handleClick=()=>{
+        alert('Custom button clicked');
+    };
+    return(
+        <section>
+            <h2>Accessibility</h2>
+            <div role="button"
+              tabIndex={0}
+              onClick={handleClick}
+              style={{padding:'20px',backgroundColor:'violet'}}>
+                Custom Accessible Button
+            </div>
+            <button onClick={handleClick}>
+                Real button
+            </button>
+        </section>
+    );
+}
