24377f9f9fde97fd8d62f1b3072af7aa564f3024
https://github.com/sinchana0913-prog/myprofile <sinchana0913@gmail.com>
2026-05-13 14:48:47 +0530

    useState and useEffect

---
+++ W11/D3/my-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import {ManagingApiState} from './components/P1.jsx'
+import{UseEffectLifecycle} from './components/P2.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <ManagingApiState /> */}
+      <UseEffectLifecycle/>
+    </>
+  )
+}
+
+export default App
