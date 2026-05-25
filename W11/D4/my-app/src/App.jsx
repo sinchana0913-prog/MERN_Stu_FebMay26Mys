e872096e3dd1dcb8d0bcf3d5a665958403bc287b
https://github.com/sinchana0913-prog/myprofile <sinchana0913@gmail.com>
2026-05-25 19:51:20 +0530

    Async form submission

--- W11/D4/my-app/src/App.jsx
+++ W11/D4/my-app/src/App.jsx
@@ -1,13 +1,16 @@
-
 import './App.css'
-import { ControlledForm } from './components/P1'
+import { ControlledForm } from "./components/P1";
+import { ValidationForm } from "./components/P2";
+import { AsyncForm } from "./components/P3";
 
 function App() {
   
-
   return (
     <>
-      <ControlledForm/>
+      {/* <ControlledForm/> */}
+      {/* <ValidationForm /> */}
+      <AsyncForm/>
+
     </>
   )
 }
