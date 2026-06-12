96eccc955c1ca410d3525f1071f8eaf8d36c1a30
https://github.com/sinchana0913-prog/myprofile <sinchana0913@gmail.com>
2026-06-12 15:20:19 +0530

    Conditional rendering

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,16 +1,14 @@
 import "./App.css";
-import BookButton from "./components/BookButton";
-import GenreFilter from "./components/GenreFilter";
-import LoginForm from "./components/LoginForm";
-import SearchShortcut from "./components/SearchShortcut";
+import AuthStatus from "./components/AuthStatus";
+import MovieLoader from "./components/MovieLoader";
+import MovieResults from "./components/MovieResults";
 
 function App() {
   return (
     <>
-      <BookButton />
-      <GenreFilter />
-      <LoginForm />
-      <SearchShortcut />
+      <AuthStatus />
+      <MovieLoader />
+      <MovieResults />
     </>
   );
 }
